4d5ea7374625fcb683e9b340913d3f62947e2b3a
MihirMehta5695 <mihirm5695@gmail.com>
2016-08-13 13:29:44 +0530

    Added various insert,delete methods along with findPosition and createList method

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -8,18 +8,21 @@ public class SinglyLinkedList
 	Node p;//The primary variable of type Node to be used for all operations regarding SinglyLinkedList
 	boolean isListExisting=false;//flag for checking existence of the list
 	boolean isListEmpty=true;//flag for checking if list is empty
+	
 	//Creating a list
 	void createList()
 	{
 
 		head=new Node();//Creating the head node which points the first node of the list (if the list exists)
+		isListEmpty=true;
+		isListExisting=true;
 
 	}//end of createList(.) method
 
 	void insertAtEnd(int value)
 	{
 
-		if(isListExisting)//Check if the list exists or not 
+		if(!isListExisting)//Check if the list exists or not 
 		{
 			System.out.println("Error: No list exists yet,Please a create list to enter values");
 			return;
@@ -47,11 +50,12 @@ public class SinglyLinkedList
 			temp=new Node(value);//creating a new Node
 			temp.next=p.next;//Assigning the value 
 			p.next=temp;//variable 'p' points towards newly created node
+			isListEmpty=false;
+			isListExisting=true;
 		}
 
 
 	}//end of insertAtEnd(.) method
-
 	
 	void insertAtBegin(int value)
 	{
@@ -60,6 +64,8 @@ public class SinglyLinkedList
 			temp=new Node(value);
 			temp.next=head.next;//making the first node of the list as the second node of the list , since we are inserting node at the beginning
 			head.next=temp;//head now points to the newly created node
+			isListEmpty=false;
+			isListExisting=true;
 		}//end of if stmt
 		
 		else
@@ -72,22 +78,11 @@ public class SinglyLinkedList
 	
 	void insertAfter(int key,int value)
 	{
-		p=head.next;//assigning the value of first node to variable 'p'
-		while(p.data!=key)
+		if(isListExisting&&!isListEmpty)
 		{
+			p=head.next;//assigning the value of first node to variable 'p'
 			
-			if(p.next==null)
-			{
-				System.out.println("Error : End of the list reached yet not found where to insert,thus inserting at the end of the list");
-				insertAtEnd(value);
-				return;
-			}//end of if stmt
-			
-		}//end of while loop
 		
-		temp=new Node(value);//create a new node
-		temp.next=p.next;//Assigning the pointer for next node
-		p.next=temp;//making p to point towards newly created node 
 		
 	}//end of insertAfter(..)
 	
@@ -116,6 +111,133 @@ public class SinglyLinkedList
 		}
 		
 	}//end of displayList() method
+		
+	void findPosition(int value)//Find the position of a node with first occurance of a particular value with respect to head node
+	{
+		if(isListExisting&&!isListEmpty)
+		{
+			int count=1;//initialising the counter variable
+			p=head.next;//initialising variable p with the first node
+			
+			while(p.next!=null)
+			{
+				
+				if(p.data==value)//if the value is found
+				{
+					System.out.println("The node with the value "+value+" is found at Position : "+count);
+					return;
+				}//end of if stmt
+				
+				else
+				{
+					p=p.next;
+					count++;
+				}//end of else stmt
+				
+				
+			}//end of while loop
+			
+			if(p.data==value)//if the value is found
+			{
+				System.out.println("The node with the value "+value+" is found at Position : "+count);
+				return;
+			}//end of if stmt
+			
+			else
+			{
+				System.out.println("Error : No such value is present in the list!");
+				return;
+			}//end of else stmt
+		
+		}//end of if stmt
+		
+		else
+		{
+			
+			System.out.println("Error: Either the list does not exist yet or either the list is empty!");
+			return;
+			
+		}//end of else stmt
+			
+	}//end of findPosition(.) method
+	
+	void deleteAtBegin()
+	{
+		if(isListExisting&&!isListEmpty)
+		{
+			
+			temp=head.next;//the node to be deleted is represented by temp;
+			head.next=temp.next;
+			temp=null;//deleting the node
+			return;
+			
+		}//end of if stmt
+		
+		else
+		{
+			System.out.println("Error: Either the list does not exists yet or the list is empty!");
+			return;
+			
+		}//end of else stmt
+			
+	}//end of deleteAtBegin() method
+
+	void deleteAtEnd()
+	{
+		if(isListExisting&&!isListEmpty)
+		{
+			
+			p=head.next;
+			while(p.next!=null)
+			{
+				temp=p;
+				p=p.next;
+			}//end of while loop
+			
+			temp.next=p.next;
+			p=null;
+			return;
+			
+		}//end of if stmt
+		
+		else
+		{
+			System.out.println("Either the list doesnot exists or the list is empty!");
+			return;
+		}//end of else stmt
+		
+	}//end of deleteAtEnd() method
+
+	void deleteAfter(int key)
+	{
+		
+		if(isListExisting&&!isListEmpty)
+		{
+			
+			p=head.next;//assigning the value of first node to variable 'p'
+			while(p.data!=key)
+			{
+				if(p.next!=null)
+				{
+					
+					
+					
+				}//end of if stmt
+				
+			}//end of while loop
+			
+		}//end of if stmt
+		
+		else
+		{
+			
+			System.out.println("Error : Either the List is not created or the Lit is Empty");
+						
+		}//end of else stmt
+		
+		
+	}//end of deleteAfter(.) method
+	
 	
 
 }//end of SinglyLinkedList class{}
